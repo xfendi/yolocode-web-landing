4495ed635737b368ae5c0cee15d721f1afe178a2
xfendi <xfendi.channel@gmail.com>
2025-02-12 17:09:49 +0100

    finished footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,22 +1,29 @@
 import React from "react";
+import Hero from "../Pages/Landing/Hero";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col gap-5">
-      <div className="text-9xl leading-none sm:text-[300px] lg:text-[500px] sm:leading-[250px] lg:leading-[400px] font-bold text-white">
-        Yolo
-      </div>
-      <div className="flex justify-between border-t pt-5 border-neutral-700">
-        <div className="text-neutral-600 text-sm text-center">
-          &copy; 2024 Yolo Code. <span className="hover:text-yellow-400 hover:underline transition-all duretion-300 cursor-pointer">Yolo</span> All rights reserved.
-        </div>
-        <div className="flex justify-center gap-5 text-sm text-neutral-600">
-          <a href="/" className="hover:text-neutral-400 hover:underline transition-all duration-300">
-            Privacy Policy
-          </a>
-          <a href="/" className="hover:text-neutral-400 hover:underline transition-all duration-300">
-            Terms of Service
-          </a>
+    <div className="p-5">
+      <div className="bg-neutral-900 rounded-3xl border-2 border-neutral-800 h-full p-5 gap-5 flex flex-col relative overflow-hidden">
+        <Hero />
+        <div className="flex justify-between">
+          <div className="text-neutral-600 text-sm text-center">
+            &copy; 2025 Yolo Code. All rights reserved.
+          </div>
+          <div className="flex justify-center gap-5 text-sm text-neutral-600">
+            <a
+              href="/"
+              className="hover:text-neutral-400 hover:underline transition-all duration-300"
+            >
+              Privacy Policy
+            </a>
+            <a
+              href="/"
+              className="hover:text-neutral-400 hover:underline transition-all duration-300"
+            >
+              Terms of Service
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/Landing/Hero.jsx
+++ src/Pages/Landing/Hero.jsx
@@ -1,24 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../../Assets/logo.png";
 
 const Hero = () => {
   return (
-    <div className="bg-[#1d1d1d] p-10 rounded-3xl flex">
-      <div className="flex-1 text-start flex flex-col gap-10">
-        <div className="text-7xl text-white font-semibold">
-          bring your <span className="text-blue-400">vision</span> to{" "}
-          <span className="text-green-400">live</span>
+    <div
+      className="flex flex-col gap-10 text-center justify-center items-center px-5 py-[15%] md:py-[5%] md:px-[10%] 2xl:px-[15%]"
+      id="hero"
+    >
+      <div className="flex flex-col gap-10 items-center" data-aos="fade-up">
+      <Link className="relative hover:-rotate-12 hover:scale-105 transition-all duration-300" to="/">
+        <img src={Logo} alt="" className="w-14" />
+      </Link>
+        <div className="text-5xl sm:text-6xl text-white text-center">
+          Bring your <span className="text-pink-400">vision</span> of a <span className="text-blue-400">dream website</span> or application to <span className="text-green-400">live</span>!
         </div>
-        <div className="text-neutral-400 text-md h-max">
-          individualized coding solutions just for you
-        </div>
+      </div>
+      <div className="flex gap-5 flex-col md:flex-row">
         <a
-          href="/"
-          className="py-2 px-5 rounded-full text-white border-2 w-max border-white hover:bg-white hover:text-black transition-all duration-300"
+          href="https://dc.yolocode.pl"
+          className="cursor-pointer p-3 px-10 text-lg font-medium w-max rounded-full transition-all duration-300 border-2 border-neutral-100 bg-neutral-100 text-black hover:border-neutral-300 hover:bg-neutral-300"
         >
-          Contact Us
+          Join Discord
         </a>
       </div>
-      <div className="flex-1 hidden lg:flex"></div>
     </div>
   );
 };
